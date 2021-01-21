3cbe61a4cd93998fe4affe676f82a02eb61d4b3f
baodoan24hdev <baodoan.24hdev@gmail.com>
2021-01-21 16:01:43 +0700

    Building a React app

---
+++ notes-app-client/src/config.js
@@ -0,0 +1,19 @@
+const config = {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-upload-bao",
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://jcentwq22d.execute-api.us-east-1.amazonaws.com/prod",
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_ZrdNix6Rz",
+    APP_CLIENT_ID: "6gh3315m1liiqpibmpg2us3fo7",
+    IDENTITY_POOL_ID: "us-east-2:99c91e40-984d-42d3-bde0-a6df6e5c7419",
+  },
+  MAX_ATTACHMENT_SIZE: 5000000,
+};
+
+export default config;
